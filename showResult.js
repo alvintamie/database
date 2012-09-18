e2e68cb46e85c4e1cfb4031943efaf46812204fe
alvi0010 <alvintamie@gmail.com>
2012-09-19 01:03:45 +0800

    Update showResult.js

--- showResult.js
+++ showResult.js
@@ -70,7 +70,7 @@ function refreshShow()
 		//drawObject(imgObject[ind],coordList[i][0], coordList[i][1]);
 		//drawText(c_obj[coordList[i][0]+":"+coordList[i][1]], coordList[i][0], coordList[i][1]);
 		addCanvasObject(showCoord[zoom][i][0],showCoord[zoom][i][1], ind);
-		if (ind == searchIndex) addCanvasObjectAuthor(getX(authorObject.city,authorObject.country),getY(authorObject.city,authorObject.country),authorIndex);
+		//if (ind == searchIndex) addCanvasObjectAuthor(getX(authorObject.city,authorObject.country),getY(authorObject.city,authorObject.country),authorIndex);
 	}
 }
 
