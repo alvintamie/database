41dd7c4e294cbe26c34da038017398e67f7edea3
alvi0010 <alvintamie@gmail.com>
2012-09-19 00:41:31 +0800

    Update showResult.js

--- showResult.js
+++ showResult.js
@@ -1,78 +1,108 @@
 var searchIndex = 15;
 var authorIndex = 10;
+var c_obj = new Array();
+var showCoord = new Array();
 
 
-function clusterObj(obj, _zoom)
+
+function clusterObj(_objArr, _zoom)
 {
-  var c_obj = new Array();
+	var _c_obj = new Array();
 	var coordList = new Array();
 	var stemp;
-	//var multiplier = 1;
-	/*
-	for (i=0; i<obj.length; ++i)
-	{
-		if (getX(obj[i].city, obj[i].country) != null)
-		{
-			stemp = getX(obj[i].city,obj[i].country).toString() +":"+ getY(obj[i].city, obj[i].country).toString();
-			if ( c_obj[stemp] ) c_obj[stemp].push(obj[i]);
-			else c_obj[stemp] = new Array(obj[i]);
-			coordList.push(new Array(getX(obj[i].city,obj[i].country),getY(obj[i].city,obj[i].country)));
-		}
-	}
-	*/
-	/*
-	for (i=0; i<obj.length; ++i)
-	{
-		if (getX(obj[i].city, obj[i].country) != null)
-		{
-			obj.x = getX(obj[i].city,obj[i].country);
-			obj.y = getY(obj[i].city,obj[i].country);
-			stemp = obj.x + ":" + obj.y;
-			if ( c_obj[stemp] ) c_obj[stemp].push(obj[i]);
-			else c_obj[stemp] = new Array(obj[i]);
-			coordList.push(new Array(getX(obj[i].city,obj[i].country),getY(obj[i].city,obj[i].country)));
-		}
-	}
-	*/
 	
-	for (i=2; i>_zoom;i--)
+	for (i=2; i>zoom;i--)
 	{
 		multiplier *= 2;
 	}
 	
 	multiplier *= 20;
 		
-	for (i=0; i<obj.length;++i)
+	for (i=0; i<_objArr.length;++i)
 	{
-		if (getX(obj[i].city, obj[i].country) != null)
+		if (getX(_objArr[i].city, _objArr[i].country) != null)
 		{
-			var x1 = Math.floor( getX(obj[i].city,obj[i].country ) / multiplier)*multiplier;
-			var y1 = Math.floor( getY(obj[i].city,obj[i].country ) / multiplier)*multiplier;
-			obj.x = x1;
-			obj.y = y1;
+			var x1 = Math.floor( getX(_objArr[i].city,_objArr[i].country ) / multiplier)*multiplier;
+			var y1 = Math.floor( getY(_objArr[i].city,_objArr[i].country ) / multiplier)*multiplier;
+			_objArr[i].x = x1;
+			_objArr[i].y = y1;
 			
-			stemp = obj.x + ":" + obj.y;
-			if (c_obj[stemp] ) c_obj[stemp].push(obj[i]);
-			else c_obj[stemp] = new Array(obj[i]);
+			stemp = _objArr[i].x + ":" + _objArr[i].y;
+			if (_c_obj[stemp] ) _c_obj[stemp].push(_objArr[i]);
+			else 
+			{
+				_c_obj[stemp] = new Array(_objArr[i]);
+				_c_obj[stemp].x = x1;
+				_c_obj[stemp].y = y1;
+			}
 			coordList.push(new Array(x1,y1));				
 		}
 	}
 	
-	return new Array(c_obj,coordList);
+	return new Array(_c_obj,coordList);
 }
 
 
-function showResult(ind, obj)
+function showResult(_ind, _objArr)
 {
-  clearCanvasObject();
-	var arr = clusterObj(obj,zoom);
-	var c_obj = arr[0];
-	var coordList = arr[1];
-	for (i=0;i<coordList.length;++i)
+	clearCanvasObject();
+	//var arr = clusterObj(obj);
+	for (i=0;i<=2;++i)
+	{
+		c_obj[i] = clusterObj(_objArr,i)[0];
+		showCoord[i] = clusterObj(_objArr,i)[1];
+	}
+	
+	for (i=0;i<showCoord[zoom].length;++i)
 	{
 		//drawObject(imgObject[ind],coordList[i][0], coordList[i][1]);
 		//drawText(c_obj[coordList[i][0]+":"+coordList[i][1]], coordList[i][0], coordList[i][1]);
-		addCanvasObject(coordList[i][0],coordList[i][1], ind);
-		if (ind == search_index) addCanvasObjectAuthor(getX(authorObject.city,authorObject.country),getY(authorObject.city,authorObject.country),authorIndex);
+		addCanvasObject(showCoord[zoom][i][0],showCoord[zoom][i][1], _ind);
+		if (_ind == searchIndex) addCanvasObjectAuthor(getX(authorObject.city,authorObject.country),getY(authorObject.city,authorObject.country),authorIndex);
 	}
 }
+
+function refreshShow()
+{
+	clearCanvasObject();
+	for (i=0;i<showCoord[zoom].length;++i)
+	{
+		//drawObject(imgObject[ind],coordList[i][0], coordList[i][1]);
+		//drawText(c_obj[coordList[i][0]+":"+coordList[i][1]], coordList[i][0], coordList[i][1]);
+		addCanvasObject(showCoord[zoom][i][0],showCoord[zoom][i][1], ind);
+		if (ind == searchIndex) addCanvasObjectAuthor(getX(authorObject.city,authorObject.country),getY(authorObject.city,authorObject.country),authorIndex);
+	}
+}
+
+function checkSquare(_clickX, _clickY, _ind, _obj)
+{
+	if (_clickX >= _obj.x && _clickX <= _obj.x + imgObject[_ind].width && _clickY >= _obj.y && _clickY <= _obj.y + imgObject[_ind].height) return  true;
+	else return false;
+}
+
+function checkCircle(_clickX, _clickY, _ind, _obj)
+{
+	var radius = img.Object[_ind].width/2;
+	var xCenter = _obj.x + radius;
+	var yCenter = _obj.y + radius;
+	if ( (_clickX-xCenter)*(_clickX-xCenter) + (_clickY-yCenter)*(_clickY-yCenter) <= radius) return true;
+	else return false;
+	
+}
+
+function listenClick(_clickX, _clickY, _ind, _obj, _check)
+{
+	if (_check(_clickX, _clickY, _ind, _obj))
+	{
+		console.log("nice click" + _obj);
+	}
+}
+
+function listenAllClick(_clickX, _clickY, _ind)
+{
+	for (obj in c_obj[zoom])
+	{
+		listenClick(_clickX, _clickY, _ind, obj, checkSquare)
+	}	
+}
+
