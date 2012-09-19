ff373704df81892ef703a4640cf912882f98b517
alvi0010 <alvintamie@gmail.com>
2012-09-19 22:39:20 +0800

    Update showResult.js

--- showResult.js
+++ showResult.js
@@ -137,8 +137,8 @@ function listenAllClick(_clickX, _clickY, _ind)
 	//console.log(c_obj);
 	//console.log(showCoord);
 	var i;
-	console.log("cX,Y: " + _clickX*multiply[zoom]+ " " + _clickY*multiply[zoom]);
-	console.log(c_obj[zoom]);
+	//console.log("cX,Y: " + _clickX*multiply[zoom]+ " " + _clickY*multiply[zoom]);
+	//console.log(c_obj[zoom]);
 	for (key in c_obj[zoom])
 	{
 		
