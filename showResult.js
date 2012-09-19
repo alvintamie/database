e589871397aaf2ec594f571fcf78a034b8139457
alvi0010 <alvintamie@gmail.com>
2012-09-19 21:40:28 +0800

    Update showResult.js

--- showResult.js
+++ showResult.js
@@ -1,54 +1,57 @@
-var searchIndex = 15;
-var authorIndex = 10;
+var searchIndex = 0;
+var authorIndex = 0;
 var c_obj = new Array();
 var showCoord = new Array();
-var multiply = new Array(4,2,1);
 
 
 
-function clusterObj(_objArr, _zoom, _ind)
+function clusterObj(_objArr, _zoom)
 {
-	var _c_obj = new Object();
+	var _c_obj = new Array();
 	var coordList = new Array();
 	var stemp;
-	var multiplier1 = 1;
-	var i = 0;
+	var multiplier = 1;
+	var i;
 	
-	for (i=2; i>_zoom;i--)
+	for (i=2; i>zoom;i--)
 	{
-		multiplier1 *= 2;
+		multiplier *= 2;
 	}
 	
-	multiplier1 *= 20;
+	multiplier *= 5;
+	var xAuth= Math.floor( getX(authorObject.city,authorObject.country)/multiplier)*multiplier;
+	var yAuth= Math.floor( getY(authorObject.city,authorObject.country)/multiplier)*multiplier;
+	authorObject.x = xAuth;
+	authorObject.y = yAuth;
+	stemp = xAuth + ":"+yAuth;
+	_c_obj[stemp] = new Array(authorObject);
+	_c_obj[stemp].x = xAuth;
+	_c_obj[stemp].y = yAuth;
+	coordList.push(new Array(xAuth, yAuth));
+	
+	
 		
 	for (i=0; i<_objArr.length;++i)
 	{
-	//console.log("hahaha " + _objArr[i].city + " " + _objArr[i].country);
-	//console.log(getObject(_objArr[i].country).getName() + " -- " + getObject(_objArr[i].city));
-		if (getX(_objArr[i].city, _objArr[i].country) != null)
+		//console.log("HAHAHA: "+_objArr[i].country +);
+		if (_objArr[i].country &&  getX(_objArr[i].city, _objArr[i].country) != null)
 		{
-			var x1 = Math.floor( getX(_objArr[i].city, _objArr[i].country) / multiplier1)*multiplier1 - Math.floor(imgObject[_ind].width/2);
-			var y1 = Math.floor( getY(_objArr[i].city,_objArr[i].country ) / multiplier1)*multiplier1 - Math.floor(imgObject[_ind].height/2);
-			//var x1 = Math.floor( getX(_objArr[i].city, _objArr[i].country));
-			//var y1 = Math.floor( getY(_objArr[i].city, _objArr[i].country));
-			//console.log(_zoom);
+			var x1 = Math.floor( getX(_objArr[i].city,_objArr[i].country ) / multiplier)*multiplier;
+			var y1 = Math.floor( getY(_objArr[i].city,_objArr[i].country ) / multiplier)*multiplier;
+			//console.log("HIHIHI " + x1);
 			_objArr[i].x = x1;
 			_objArr[i].y = y1;
 			
 			stemp = _objArr[i].x + ":" + _objArr[i].y;
-			if (_c_obj[stemp] )
-			{
-				_c_obj[stemp].push(_objArr[i]);
-				//_c_obj[stemp][_c_obj[stemp].length-1].zoom = _zoom;
-			}
+			if (_c_obj[stemp] ) _c_obj[stemp].push(_objArr[i]);
 			else 
 			{
 				_c_obj[stemp] = new Array(_objArr[i]);
 				_c_obj[stemp].x = x1;
 				_c_obj[stemp].y = y1;
-				//_c_obj[stemp][0].zoom = _zoom;
+				coordList.push(new Array(x1,y1));
 			}
-			coordList.push(new Array(x1,y1));				
+							
 		}
 	}
 	
@@ -58,47 +61,81 @@ function clusterObj(_objArr, _zoom, _ind)
 
 function showResult(_ind, _objArr)
 {
+	var i;
 	clearCanvasObject();
-	//console.log(_objArr);
+	canvasObjectText = [];
+	//console.log("HIHIHIHIHIHI");
+	
 	//var arr = clusterObj(obj);
-	var i=0;
 	for (i=0;i<=2;++i)
 	{
-		var objTemp = new Object();
-		//var objTemp = jQuery.extend(true, {}, _objArr);
-		//console.log(i);
-		c_obj.push(clusterObj(_objArr,i, _ind)[0]);
-		showCoord.push(clusterObj(_objArr,i, _ind)[1]);
-		//console.log (clusterObj(_objArr,i));
+		c_obj.push(clusterObj(_objArr,i)[0]);
+		showCoord.push(clusterObj(_objArr,i)[1]);
 	}
-	console.log(c_obj);
-	for (i=0;i<showCoord[zoom].length;++i)
+	
+	for (i=1;i<showCoord[zoom].length;++i)
 	{
 		//drawObject(imgObject[ind],coordList[i][0], coordList[i][1]);
 		//drawText(c_obj[coordList[i][0]+":"+coordList[i][1]], coordList[i][0], coordList[i][1]);
-		//console.log("x: " + showCoord[zoom][i][0]+"y: " +  showCoord[zoom][i][1] + "ind: " + _ind);
-		addCanvasObject(showCoord[zoom][i][0]/4,showCoord[zoom][i][1]/4, 0);
-		//addCanvasObject(10,10, 0);
-
-		//if (_ind == searchIndex) addCanvasObjectAuthor(getX(authorObject.city,authorObject.country),getY(authorObject.city,authorObject.country),authorIndex);
+		addCanvasObject(showCoord[zoom][i][0]/4,showCoord[zoom][i][1]/4, _ind);
+		canvasObjectText.push(c_obj[zoom][showCoord[zoom][i][0]+":"+showCoord[zoom][i][1]].length)		
 	}
+	addCanvasObjectAuthor(showCoord[zoom][0][0]/4,showCoord[zoom][0][1]/4, authorIndex)
+	canvasObjectText.push(c_obj[zoom][showCoord[zoom][0][0]+":"+showCoord[zoom][0][1]].length)
 }
 
 function refreshShow()
 {
-	ind = 0;
+	var i;
 	clearCanvasObject();
-	console.log(showCoord[zoom].length);
+	canvasObjectText = [];
 	for (i=0;i<showCoord[zoom].length;++i)
 	{
 		//drawObject(imgObject[ind],coordList[i][0], coordList[i][1]);
 		//drawText(c_obj[coordList[i][0]+":"+coordList[i][1]], coordList[i][0], coordList[i][1]);
-		//console.log(zoom);
-		addCanvasObject(showCoord[zoom][i][0]/4,showCoord[zoom][i][1]/4, ind);
-		//if (ind == searchIndex) addCanvasObjectAuthor(getX(authorObject.city,authorObject.country),getY(authorObject.city,authorObject.country),authorIndex);
+		addCanvasObject(showCoord[zoom][i][0]/4,showCoord[zoom][i][1]/4, 0);
+		canvasObjectText.push(c_obj[zoom][showCoord[zoom][i][0]+":"+showCoord[zoom][i][1]].length)
+	//	if (ind == searchIndex) addCanvasObjectAuthor(getX(authorObject.city,authorObject.country)/4,getY(authorObject.city,authorObject.country)/4,authorIndex);
+	
+
 	}
+	addCanvasObjectAuthor(showCoord[zoom][0][0]/4,showCoord[zoom][0][1]/4, authorIndex)
+	canvasObjectText.push(c_obj[zoom][showCoord[zoom][0][0]+":"+showCoord[zoom][0][1]].length)
+}
+
+function checkSquare(_clickX, _clickY, _ind, _obj)
+{
+	if (_clickX >= _obj.x && _clickX <= _obj.x + imgObject[_ind].width && _clickY >= _obj.y && _clickY <= _obj.y + imgObject[_ind].height) return  true;
+	else return false;
+}
+
+function checkCircle(_clickX, _clickY, _ind, _obj)
+{
+	var radius = img.Object[_ind].width/2;
+	var xCenter = _obj.x + radius;
+	var yCenter = _obj.y + radius;
+	if ( (_clickX-xCenter)*(_clickX-xCenter) + (_clickY-yCenter)*(_clickY-yCenter) <= radius) return true;
+	else return false;
 	
 }
+
+function listenClick(_clickX, _clickY, _ind, _obj, _check)
+{
+	if (_check(_clickX, _clickY, _ind, _obj))
+	{
+		console.log("nice click" + _obj);
+	}
+}
+
+function listenAllClick(_clickX, _clickY, _ind)
+{
+	console.log("listen Jalan");
+	for (obj in c_obj[zoom])
+	{
+		listenClick(_clickX, _clickY, _ind, obj, checkSquare)
+	}	
+}
+
 
 function checkSquare(_clickX, _clickY, _ind, _obj)
 {
@@ -134,7 +171,7 @@ function listenAllClick(_clickX, _clickY, _ind)
 	
 	for (key in c_obj[zoom])
 	{
-		//console.log("cX,Y: " + _clickX*multiplier1 + " " + _clickY*multiplier1 + "objX,Y: " + c_obj[zoom][key].x + " " + c_obj[zoom][key].y);
+		//console.log("cX,Y: " + _clickX*multiplier + " " + _clickY*multiplier + "objX,Y: " + c_obj[zoom][key].x + " " + c_obj[zoom][key].y);
 		//console.log(_ind);
 		listenClick(_clickX, _clickY, _ind, c_obj[zoom][key], checkSquare)
 	}	
