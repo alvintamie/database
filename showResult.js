2ad88ba3aab835bbfa43dd2437923e00d48d919c
alvi0010 <alvintamie@gmail.com>
2012-09-23 06:59:16 +0800

    Update showResult.js

--- showResult.js
+++ showResult.js
@@ -42,7 +42,7 @@ function clusterObj(_objArr, _zoom, _ind)
 		multiplier1 *= 2;
 	}
 	
-	multiplier1 *= 40;
+	multiplier1 *= 35;
 	
 	if (typeof(authorObject.city) == 'undefined' ) authorObject.city = "noCity";
 	if (typeof(authorObject.hitCount) == 'undefined') authorObject.hitCount = 1;
